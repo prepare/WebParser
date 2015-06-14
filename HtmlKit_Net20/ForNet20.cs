2c79f83a2bd5c688a600b55a861b2f5a0df59242
win <wintercoredev@gmail.com>
2015-06-14 18:34:34 +0700

    01: For those who stick with .Net20 :)

---
+++ HtmlKit_Net20/ForNet20.cs
@@ -0,0 +1,18 @@
+﻿#define NET20 
+#if NET20
+namespace System.Runtime.CompilerServices
+{
+    public class ExtensionAttribute { }
+}
+namespace System.Text
+{
+    public static class StringBuilderExtension
+    {
+        public static void Clear(this StringBuilder stbuilder)
+        {
+            stbuilder.Length = 0;
+        }
+    } 
+}
+
+#endif
